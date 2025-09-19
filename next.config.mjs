faa7beef87aaa286930698e161f5526ded05253b
genZ <kimchanthon46@gmail.com>
2025-09-19 23:08:30 +0700

    add ci-cd 1

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: 'export',           // crucial for static export
     images: { unoptimized: true }, // GH Pages lacks the image optimizer
-    basePath: 'https://github.com/ThornieDev/GenZPortfolio',  // uncomment if deploying to user.github.io/<repo-name>
+    basePath: 'https://github.com/ThornieDev/thornieportfolio',  // uncomment if deploying to user.github.io/<repo-name>
 };
 
 export default nextConfig;
