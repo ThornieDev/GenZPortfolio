8b41bed346d53b9626d565e2d2cff8f5813c9b4f
genZ <kimchanthon46@gmail.com>
2025-09-19 23:02:39 +0700

    add ci-cd

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',           // crucial for static export
+    images: { unoptimized: true }, // GH Pages lacks the image optimizer
+    basePath: 'https://github.com/ThornieDev/GenZPortfolio',  // uncomment if deploying to user.github.io/<repo-name>
+};
 
 export default nextConfig;
